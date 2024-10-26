8cc50bfc7f1eec7f733b9c3778091eddb8480e2a
Eduardo Zborowski <duduuks@live.com>
2024-10-25 23:32:06 -0300

    Fix peers

--- src/rede/p2p.rs
+++ src/rede/p2p.rs
@@ -1,13 +1,14 @@
 use libp2p::{
     development_transport,
-    gossipsub::{Gossipsub, GossipsubEvent, IdentTopic as Topic, MessageAuthenticity},
-    identity, swarm::{SwarmEvent}, PeerId, Swarm, Multiaddr, NetworkBehaviour
+    gossipsub::{
+        Gossipsub, GossipsubEvent, IdentTopic as Topic,
+        MessageAuthenticity, GossipsubConfig,
+    },
+    identity, PeerId, Swarm, Multiaddr, NetworkBehaviour,
 };
-use tokio::sync::{mpsc, Mutex};
-use futures::StreamExt;
-use serde_json::Value;
+use futures::prelude::*;
+use serde_json;
 use crate::blockchain::{Bloco, Transacao};
-use std::sync::Arc;
 
 #[derive(NetworkBehaviour)]
 #[behaviour(out_event = "MyBehaviourEvent")]
@@ -26,18 +27,6 @@ impl From<GossipsubEvent> for MyBehaviourEvent {
     }
 }
 
-impl MyBehaviour {
-    pub async fn new(id_keys: identity::Keypair) -> Self {
-        let gossipsub_config = libp2p::gossipsub::GossipsubConfig::default();
-        let gossipsub: Gossipsub = Gossipsub::new(
-            MessageAuthenticity::Signed(id_keys.clone()),
-            gossipsub_config,
-        ).unwrap();
-
-        MyBehaviour { gossipsub }
-    }
-}
-
 pub enum P2PEvent {
     NovoBloco(Bloco),
     NovaTransacao(Transacao),
@@ -46,26 +35,27 @@ pub enum P2PEvent {
 pub struct P2PSwarm {
     pub swarm: Swarm<MyBehaviour>,
     topic: Topic,
-    peer_id: PeerId,
-    sender: mpsc::Sender<P2PEvent>,
 }
 
 impl P2PSwarm {
-    pub async fn new(
-        blockchain: Arc<Mutex<crate::blockchain::Blockchain>>
-    ) -> (Self, mpsc::Receiver<P2PEvent>) {
+    pub async fn new() -> Self {
         let local_key = identity::Keypair::generate_ed25519();
         let peer_id = PeerId::from(local_key.public());
 
         println!("Peer ID local: {}", peer_id);
 
         let transport = development_transport(local_key.clone()).await.unwrap();
-        let mut behaviour = MyBehaviour::new(local_key.clone()).await;
+        let gossipsub_config = GossipsubConfig::default();
+        let mut gossipsub = Gossipsub::new(
+            MessageAuthenticity::Signed(local_key.clone()),
+            gossipsub_config,
+        )
+            .unwrap();
 
         let topic = Topic::new("blockchain");
-        behaviour.gossipsub.subscribe(&topic).unwrap();
+        gossipsub.subscribe(&topic).unwrap();
 
-        let mut swarm = Swarm::new(transport, behaviour, peer_id.clone());
+        let mut swarm = Swarm::new(transport, MyBehaviour { gossipsub }, peer_id);
 
         // Configuração de porta
         let porta = if std::env::args().any(|arg| arg == "--autoridade") {
@@ -73,34 +63,38 @@ impl P2PSwarm {
         } else {
             "4002"
         };
-        swarm.listen_on(format!("/ip4/0.0.0.0/tcp/{}", porta).parse().unwrap()).unwrap();
+        swarm
+            .listen_on(format!("/ip4/0.0.0.0/tcp/{}", porta).parse().unwrap())
+            .unwrap();
 
-        // Configuração de conexão com peer conhecido
-        let peer_address = if porta == "4001" {
+        // Conecta ao peer conhecido
+        let outro_peer = if porta == "4001" {
             "/ip4/127.0.0.1/tcp/4002"
         } else {
             "/ip4/127.0.0.1/tcp/4001"
         };
-        swarm.dial(peer_address.parse::<Multiaddr>().unwrap()).expect("Falha ao conectar com peer");
+        swarm
+            .dial(outro_peer.parse::<Multiaddr>().unwrap())
+            .unwrap_or_else(|e| println!("Falha ao conectar com peer: {:?}", e));
 
-        let (sender, receiver) = mpsc::channel(100);
-
-        (Self { swarm, topic, peer_id, sender }, receiver)
+        P2PSwarm { swarm, topic }
     }
 
-    pub async fn processar_eventos(&mut self) {
-        while let Some(event) = self.swarm.next().await {
-            match event {
-                SwarmEvent::NewListenAddr { address, .. } => {
-                    println!("Escutando em {:?}", address);
-                }
-                SwarmEvent::Behaviour(event) => match event {
-                    MyBehaviourEvent::Gossipsub(GossipsubEvent::Message { message, .. }) => {
-                        let data_str = String::from_utf8_lossy(&message.data);
-                        println!("Mensagem recebida: {}", data_str);
+    pub async fn next_event(&mut self) -> Option<P2PEvent> {
+        loop {
+            match self.swarm.select_next_some().await {
+                libp2p::swarm::SwarmEvent::Behaviour(MyBehaviourEvent::Gossipsub(
+                                                         GossipsubEvent::Message { message, .. },
+                                                     )) => {
+                    let data_str = String::from_utf8_lossy(&message.data);
+                    if let Ok(bloco) = serde_json::from_str::<Bloco>(&data_str) {
+                        return Some(P2PEvent::NovoBloco(bloco));
+                    } else if let Ok(transacao) =
+                        serde_json::from_str::<Transacao>(&data_str)
+                    {
+                        return Some(P2PEvent::NovaTransacao(transacao));
                     }
-                    _ => {}
-                },
+                }
                 _ => {}
             }
         }
@@ -108,25 +102,25 @@ impl P2PSwarm {
 
     pub fn difundir_transacao(&mut self, transacao: &Transacao) {
         let data = serde_json::to_string(transacao).unwrap();
-        self.swarm
+        if let Err(e) = self.swarm
             .behaviour_mut()
             .gossipsub
-            .publish(self.topic.clone(), data.as_bytes())
-            .unwrap();
+            .publish(self.topic.clone(), data.as_bytes()) {
+            println!("Erro ao difundir transação: {:?}", e);
+        }
     }
 
     pub fn difundir_bloco(&mut self, bloco: &Bloco) {
         let data = serde_json::to_string(bloco).unwrap();
-        self.swarm
+        if let Err(e) = self.swarm
             .behaviour_mut()
             .gossipsub
-            .publish(self.topic.clone(), data.as_bytes())
-            .unwrap();
+            .publish(self.topic.clone(), data.as_bytes()) {
+            println!("Erro ao difundir bloco: {:?}", e);
+        }
     }
 }
 
-pub async fn iniciar_rede(
-    blockchain: Arc<tokio::sync::Mutex<crate::blockchain::Blockchain>>
-) -> (P2PSwarm, mpsc::Receiver<P2PEvent>) {
-    P2PSwarm::new(blockchain).await
+pub async fn iniciar_rede() -> P2PSwarm {
+    P2PSwarm::new().await
 }
